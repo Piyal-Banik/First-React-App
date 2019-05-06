2d984ecd8998f36c1ff5406a27e43e20ca31c0c7
Piyal Banik <piyalbanik2@gmail.com>
2019-05-06 11:45:12 +0530

    Added Container component MainComponent.js and made DishdetailComponent and MenuComponent as Presentational component.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,24 +2,14 @@ import React, {Component} from 'react'
 import {Card,CardImg,CardText,CardBody,CardTitle} from 'reactstrap'
 
 class DishDetail extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            dish: props.dishSelected,
-        }
-    }
-
-    componentWillReceiveProps(props) {
-        this.setState({dish: props.dishSelected});
-    }
-
     renderComments(comments) {
         const comment = comments.map((eachComment) => {
             return (
                 <ul className = "list-unstyled" key={eachComment.id}>
                     <p>{eachComment.comment}</p>
-                    <p>--{eachComment.author}, {eachComment.date}</p>
+                    <p>--{eachComment.author},  
+                        {new Intl.DateTimeFormat('en-US', {year: 'numeric', month : 'short', day : '2-digit'}).format(
+                            new Date(Date.parse(eachComment.date)))}</p>
                 </ul>
             );
         });
@@ -27,31 +17,40 @@ class DishDetail extends Component {
         return comment;
     }
 
+    renderDish(dish) {
+        return(
+            <Card>
+                <CardImg width="100%" object src = {this.props.dish.image} alt={this.props.dish.name} />
+                <CardBody>
+                    <CardTitle>
+                        {this.props.dish.name}
+                    </CardTitle>
+                    <CardText>
+                        {this.props.dish.description}
+                    </CardText>
+                </CardBody>
+            </Card>
+        );
+        
+    }
+
     render () {
-        if(this.state.dish != null) {
+        if(this.props.dish != null) {
             return (
-                <div className = "row">
-                    <div className ="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" object src = {this.state.dish.image} alt={this.state.dish.name} />
-                            <CardBody>
-                                <CardTitle>
-                                    {this.state.dish.name}
-                                </CardTitle>
-                                <CardText>
-                                    {this.state.dish.description}
-                                </CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className = "col-12 col-md-5 m-1">
-                        <div>
-                            <h4>Comments</h4>
+                <div className = "container">
+                    <div className = "row">
+                        <div className ="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
                         </div>
-                        <div>
-                            {this.renderComments(this.state.dish.comments)}
+                        <div className = "col-12 col-md-5 m-1">
+                            <div>
+                                <h4>Comments</h4>
+                            </div>
+                            <div>
+                                {this.renderComments(this.props.dish.comments)}
+                            </div>
                         </div>
-                    </div>
+                    </div>      
                 </div>
             );
         }
